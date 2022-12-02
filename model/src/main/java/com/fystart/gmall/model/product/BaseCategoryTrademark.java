ceb0f63e3f80a7e1858e91eeb30f08f29bca9012
fy <fystart.163.com>
2022-12-02 22:46:48 +0800

    进一步初始化项目

---
+++ model/src/main/java/com/fystart/gmall/model/product/BaseCategoryTrademark.java
@@ -0,0 +1,36 @@
+//
+//
+package com.fystart.gmall.model.product;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import com.fystart.gmall.model.base.BaseEntity;
+
+/**
+ * <p>
+ * BaseCategoryView
+ * </p>
+ *
+ */
+@Data
+@ApiModel(description = "BaseCategoryTrademark")
+@TableName("base_category_trademark")
+public class BaseCategoryTrademark extends BaseEntity {
+
+	private static final long serialVersionUID = 1L;
+
+	@ApiModelProperty(value = "三级分类编号")
+	@TableField("category3_id")
+	private Long category3Id;
+
+	@ApiModelProperty(value = "品牌id")
+	@TableField("trademark_id")
+	private Long trademarkId;
+
+	@TableField(exist = false)
+	private BaseTrademark baseTrademark;
+}
+
